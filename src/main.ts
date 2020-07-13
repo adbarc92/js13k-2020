19c2ac0c3cc1f0dd1cacb11ca4352509286f8875
adbarc92 <adbarclay92@gmail.com>
2020-07-12 20:12:00 -0600

    Adds dependencies, adds build command to compile to TypeScript

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+const doc =
+  document.getElementById('root') && document.getElementById('root')?.innerHTML
+    ? 'I like pizza'
+    : 'pizza sucks';
+
+const app = () => {
+  console.log('sdlfjkahsdljkfhasdjf');
+};
