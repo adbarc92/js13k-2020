237a5ad24ed3cb89ab1f6653c87cae30cb51d0d8
adbarc92 <adbarclay92@gmail.com>
2020-09-11 16:27:37 -0600

    Adds two new rooms and characters for the rooms

--- src/controller/interact.ts
+++ src/controller/interact.ts
@@ -34,3 +34,15 @@ const G_controller_playLinearCutscene = async (lines: string[]) => {
   }
   G_view_playSound('menuCancel');
 };
+
+const G_controller_facePlayer = (character: Character) => {
+  const world = G_model_getCurrentWorld();
+  const protag = G_model_partyGetProtag(world.party);
+  const protagActor = protag.actor;
+  const characterActor = character.actor;
+  if (protagActor.x < characterActor.x) {
+    G_model_actorSetFacing(characterActor, G_FACING_LEFT);
+  } else {
+    G_model_actorSetFacing(characterActor, G_FACING_RIGHT);
+  }
+};
